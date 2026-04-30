f336e61cf3471931a12ac4441295db4b53d8a4af
taeoukkim <taeoukkim@users.noreply.github.com>
2026-04-30 23:50:11 +0000

    deploy: 17784383e92d7c1285610474f3a3936b787b00aa

--- search/variables_e.js
+++ search/variables_e.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['q_5fprev_5fsent_0',['Q_prev_sent',['../structsv_one_d_1_1_one_d_model_state.html#a2d158490342b96e67862fd05ae5f74e5',1,'svOneD::OneDModelState']]],
-  ['qmtet4_1',['qmTET4',['../classmsh_type.html#afa936d36a92a9d1f218fd17e34454a9a',1,'mshType']]]
+  ['q_5fprev_5fsent_5fnew_1',['Q_prev_sent_new',['../structsv_one_d_1_1_one_d_model_state.html#a545d47491d83a89f7622647f26bf5b96',1,'svOneD::OneDModelState']]],
+  ['q_5fprev_5fsent_5fold_2',['Q_prev_sent_old',['../structsv_one_d_1_1_one_d_model_state.html#ab3d95c0588a08b6ea1c70202ce845864',1,'svOneD::OneDModelState']]],
+  ['qmtet4_3',['qmTET4',['../classmsh_type.html#afa936d36a92a9d1f218fd17e34454a9a',1,'mshType']]]
 ];
